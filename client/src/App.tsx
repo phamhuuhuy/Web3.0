c272cc7769be1da0f179a994a08af7e6bce18079
phamhuuhuy <huykiengabc9@gmail.com>
2023-02-21 21:25:27 +0700

    import tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <h1 className="text-3xl font-bold underline"> Hello world!</h1>;
+  return <p className="text-3xl font-bold underline"> Hello world!</p>;
 };
 
 export default App;
